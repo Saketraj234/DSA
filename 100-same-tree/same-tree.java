5680150a675eadcef8922b2779d3676a59d09c11
Saket Raj <saketbarnwal468@gmail.com>
2026-06-28 21:26:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (51.29%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -1,32 +1,3 @@
-/**
- * Problem: 100. Same Tree
- *
- * Given the roots of two binary trees p and q,
- * return true if they are the same,
- * otherwise return false.
- *
- * Two binary trees are considered the same if:
- * 1. They have the same structure.
- * 2. Corresponding nodes have the same value.
- *
- * Approach:
- * We use Recursion (DFS).
- *
- * Base Cases:
- * 1. If both nodes are NULL -> return true.
- * 2. If one node is NULL -> return false.
- * 3. If values are different -> return false.
- *
- * Otherwise:
- * - Compare left subtree.
- * - Compare right subtree.
- * - Return true only if both are same.
- *
- * Time Complexity: O(n)
- * Space Complexity: O(h)
- * where h = height of the tree.
- */
-
 class Solution {
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
