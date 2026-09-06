63eb7009c0150c1ae7b901dd02a42cbb3def4dce
Saket Raj <saketbarnwal468@gmail.com>
2026-09-06 23:11:16 +0530

    Time: 4 ms (99.55%) | Memory: 47 MB (92.15%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -1,71 +1,31 @@
-/**
- * Problem: 463. Island Perimeter
- *
- * Given a grid where:
- * 0 = water
- * 1 = land
- *
- * return the perimeter of the island.
- *
- * Approach:
- * We use DFS.
- *
- * For every land cell:
- * - If we go outside the grid → +1 perimeter.
- * - If we reach water (0) → +1 perimeter.
- * - If the cell is already visited → +0.
- * - Otherwise, mark it visited and visit all 4 directions.
- *
- * We mark visited cells as -1 directly in the grid,
- * so we don't need an extra visited[][] array.
- *
- * Directions:
- * up    → (i - 1, j)
- * down  → (i + 1, j)
- * left  → (i, j - 1)
- * right → (i, j + 1)
- *
- * Time Complexity: O(m * n)
- * Space Complexity: O(m * n) due to recursion stack
- */
-
 class Solution {
     public int islandPerimeter(int[][] grid) {
+
         int row = grid.length;
         int col = grid[0].length;
 
-        for(int i = 0; i < grid.length; i++){
+        int perimeter = 0;
 
-        for(int j = 0; j < grid[0].length; j++){
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < col; j++) {
 
-            if(grid[i][j] == 1){
-               return dfs(grid, i, j);
+                if(grid[i][j] == 1) {
+
+                    if(i == 0 || grid[i - 1][j] == 0)
+                        perimeter++;
+
+                    if(i == row - 1 || grid[i + 1][j] == 0)
+                        perimeter++;
+
+                    if(j == 0 || grid[i][j - 1] == 0)
+                        perimeter++;
+
+                    if(j == col - 1 || grid[i][j + 1] == 0)
+                        perimeter++;
+                }
             }
         }
+
+        return perimeter;
     }
-    return 0;
 }
-
-        public int dfs(int[][] grid, int i, int j){
-
-            if(i < 0 || i >= grid.length ||
-            j < 0 || j >= grid[0].length){
-                return 1;
-            }
-
-            if(grid[i][j] == 0){
-                return 1;
-            }
-
-            if(grid[i][j] == -1){
-            return 0;
-            }
-
-            grid[i][j] = -1;
-
-            return dfs(grid, i - 1, j)
-            + dfs(grid, i + 1, j)
-            + dfs(grid, i, j - 1)
-            + dfs(grid, i, j + 1);
-        }
-    }
