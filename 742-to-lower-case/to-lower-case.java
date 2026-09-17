aa3be2c42a7903804c2d6dbfb45bf768807f9495
Saket Raj <saketbarnwal468@gmail.com>
2026-09-18 00:22:52 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (19.52%) - LeetSync

--- 742-to-lower-case/to-lower-case.java
+++ 742-to-lower-case/to-lower-case.java
@@ -1,35 +1,7 @@
-/**
- * Problem: 709. To Lower Case
- *
- * Given a string s, return the string after converting
- * all uppercase letters into lowercase letters.
- *
- * Approach:
- * We traverse the string character by character.
- *
- * If the character is between 'A' and 'Z',
- * convert it into lowercase by adding 32.
- *
- * Otherwise, keep the character unchanged.
- *
- * We use StringBuilder to build the answer.
- *
- * Time Complexity: O(n)
- * Space Complexity: O(n)
- */
-
+// Most OPtimized
 class Solution {
     public String toLowerCase(String s) {
         
-        StringBuilder result = new StringBuilder();
-
-        for(char ch : s.toCharArray()){
-
-            if(ch >= 'A' && ch <= 'Z'){
-                ch = (char)(ch + 32);
-            }
-            result.append(ch);
-        }
-        return result.toString();
+        return s.toLowerCase();
     }
 }
